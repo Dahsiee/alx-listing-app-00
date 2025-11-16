49893709dc38cb9f76e0ddac49275434bf35c1ed
Davcid1572 <ifejiofordavid1572@gmail.com>
2025-11-16 08:48:08 +0100

    Working on the header component

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import Image from "next/image";
+import Navbar from "../common/Navbar";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="w-full overflow-x-hidden">
+      <div className="bg-[#36927A] w-full py-3 mx-auto flex flex-wrap items-center justify-center gap-4 text-white px-4">
+        <Image
+          src="/assets/images/icons/message.png"
+          alt="Message Icon"
+          width={20}
+          height={20}
+        />
+
+        <p className="text-sm text-center">
+          Overseas trip? Get the latest information on travel guides
+        </p>
+
+        <button className="bg-black text-white px-4 py-1 rounded-full text-sm shrink-0">
+          More Info
+        </button>
+      </div>
+
+      <Navbar />
+    </div>
+  );
 };
 
 export default Header;
